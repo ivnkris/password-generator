d2a40f81bcac852aa5839e4aff14d901621bb825
Krisztian Ivan <krisztian.ivan@gmail.com>
2021-03-14 11:09:59 +0000

    added logic in pseudo code

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+//prompt for the length of the password stored in a variable
+
+//validation if the password length is between 8 and 128 characters
+
+//confirmation to include lowercase, uppercase, numeric and special characters stored in a boolean variable
+
+//validation if at least one character type was selected
+
+//function to push at least one random character of each selected character type into the final password array
+
+//function to concatenate arrays for selected characters and set loop length depending on how many characters were pushed already
+
+//function to loop through concatenated character array and push random characters into password array
+
+//function to join password array into a string
+
 function generatePassword() {
   var password = "";
 
